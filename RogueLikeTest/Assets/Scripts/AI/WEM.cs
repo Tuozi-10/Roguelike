930e3add342c1e7aecb794744244be59f1f0ec89
Mrcookie <hautcoeur.corentin@gmail.com>
2025-02-13 10:11:19 +0100

    WEM monster ( Spawn still bugged )

--- RogueLikeTest/Assets/Scripts/AI/WEM.cs
+++ RogueLikeTest/Assets/Scripts/AI/WEM.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
 using UnityEngine;
+using DG.Tweening;
 using Random = UnityEngine.Random;
 
 namespace AI
 {
     [RequireComponent(typeof(HitCollider))]
-    public class Bat : AbstractIA
+    public class WEM : AbstractIA
     {
         [Header("Specific values"), Space]
         [SerializeField] private float m_rangeWander = 2;
@@ -17,12 +19,19 @@ namespace AI
 
         private BatDataInstance m_batData;
         
+        [Header("Spawn part"), Space]
+        [SerializeField] private List<AbstractIA> m_spawn;
+        
+        [SerializeField] private int m_spawnMax = 5;
+        [SerializeField] private int m_spawnMin = 1;
+        
         protected override void Init()
         {
             base.Init();
             basePosition = transform.position;
             
             m_batData = (BatDataInstance)m_dataInstance;
+            
         }
 
         public override void ChangeState(AIStates aiState)
@@ -44,7 +53,7 @@ namespace AI
             m_moving = true;
             
             Vector2 newMoveTarget = new Vector2(Random.Range(basePosition.x - m_rangeWander, basePosition.x + m_rangeWander),
-                    Random.Range(basePosition.y - m_rangeWander, basePosition.y + m_rangeWander));
+                Random.Range(basePosition.y - m_rangeWander, basePosition.y + m_rangeWander));
             
             m_transform.DOMove(newMoveTarget, 1f).OnComplete(() => m_moving = false);
         }
@@ -62,6 +71,26 @@ namespace AI
         {
             if(other.transform.GetComponent<DingIa>() != null)
                 Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), other.collider);
+            
         }
+        
+        IEnumerator<WaitForSeconds> WEMcoroutine()
+        {
+            DoCageShoot();
+            yield return new WaitForSeconds(5f);
+        }
+        public void DoCageShoot()
+        {
+            for (int i = 0; i < Random.Range(m_spawnMin, m_spawnMax + 1); i++)
+            {
+                var go = Instantiate(m_spawn[Random.Range(0, m_spawn.Count)], transform.parent, true);
+                go.transform.position = transform.position + new Vector3(Random.Range(-0.75f, 0.75f), Random.Range(-0.75f,0.75f),0);
+                go.GetComponent<AbstractIA>().ChangeState(AIStates.attacking);
+            }
+            
+        }
+
+        
     }
 }
+
